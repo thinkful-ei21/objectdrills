70f0ccf11d2a0dbe467a465db711e6b59e7e01a5
Brian Dixon <briandixon@Brians-Mac-mini.home>
2018-05-09 15:30:07 -0400

    5th push

--- objdrill3.js
+++ objdrill3.js
@@ -9,3 +9,24 @@ let loaf = {
 };
 console.log(`flour: ${loaf.flour}, water: ${loaf.water}`);
 console.log(`Loaf hydration: ${loaf.hydration()}`);
+
+
+
+let object = {
+	foo: 1,
+	bar: 2,
+	fum: 13,
+	quux: 92,
+	spam: 14
+}
+
+function stuff(object) {
+	var text = " ";
+	var x;
+	 for (x in object) {
+			text += x + " " + object[x] + " " ;
+		}
+	return text
+}
+
+console.log(stuff(object));
