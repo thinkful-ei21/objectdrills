513ad999808c59e5b0baea663ea15be54bdad230
Brian Dixon <briandixon@Brians-Mac-mini.home>
2018-05-09 11:40:22 -0400

    first push

---
+++ objdrill.js
@@ -0,0 +1,15 @@
+function createMyObject() {
+  // your code here
+  const objs = {
+    foo: "bar",
+    answerToUniverse: 42,
+    olly olly: `oxen free`,
+    sayHello: function(){
+      return `hello`
+    }
+      return objs
+
+  }
+}
+
+function createMyObject();
